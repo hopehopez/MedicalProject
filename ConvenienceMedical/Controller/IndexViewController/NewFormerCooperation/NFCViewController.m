187a5d2da677ff55f3c6b8b3992bb27cfbd48c67
zhangshuqing <zhangshuqing912@163.com>
2016-01-23 19:17:42 +0800

    add NewFormerCoopertion  part

---
+++ ConvenienceMedical/Controller/IndexViewController/NewFormerCooperation/NFCViewController.m
@@ -0,0 +1,37 @@
+//
+//  NFCViewController.m
+//  ConvenienceMedical
+//
+//  Created by 张树青 on 16/1/23.
+//  Copyright (c) 2016年 zsq. All rights reserved.
+//
+
+#import "NFCViewController.h"
+
+@interface NFCViewController ()
+
+@end
+
+@implementation NFCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
